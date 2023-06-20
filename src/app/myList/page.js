105145e0c2e9e7c9d42dedbf8c0ffcae8a4bc72d
Lopezzz099 <nacholopez1903@gmail.com>
2023-06-20 13:03:30 -0300

    configurando mylist

--- src/app/myList/page.js
+++ src/app/myList/page.js
@@ -2,19 +2,39 @@
 
 import CardMovie from "@/components/common/cardMovie/CardMovie";
 import { useSelector } from "react-redux";
+import { AiFillPlusCircle } from "react-icons/ai";
 
 const MyList = () => {
   const { myList } = useSelector((store) => store.favSlice);
 
   return (
-    <div style={{ paddingTop: "100px" }}>
-      <h1>favoritos</h1>
-      <div className="flex flex-wrap">
-        {myList.map((movie) => (
-          <CardMovie key={movie.id} movie={movie} />
-        ))}
-      </div>
-    </div>
+    <>
+      {myList.length === 0 && (
+        <div
+          className="flex items-center justify-center"
+          style={{ paddingTop: "100px" }}
+        >
+          <div
+          className="flex items-center justify-center"
+            style={{ backgroundColor: "green", width: "100%", height: "100%" }}
+          >
+            <AiFillPlusCircle style={{ width: "120px", height: "120px" }} />
+          </div>
+        </div>
+      )}
+      {
+        <div style={{ paddingTop: "100px" }}>
+          <div
+            className="grid grid-cols-5 gap-7"
+            style={{ padding: "0 calc(3.5vw + 24px)" }}
+          >
+            {myList.map((movie) => (
+              <CardMovie key={movie.id} movie={movie} />
+            ))}
+          </div>
+        </div>
+      }
+    </>
   );
 };
 
