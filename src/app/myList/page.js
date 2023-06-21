49dbdd9c34422cc6ed414a1abdbc29b182f3cf81
Lopezzz099 <nacholopez1903@gmail.com>
2023-06-21 14:16:32 -0300

    agregandole minHeight en myList

--- src/app/myList/page.js
+++ src/app/myList/page.js
@@ -10,8 +10,8 @@ const MyList = () => {
   return (
       myList.length === 0 ? (
         <div
-          className="flex items-center justify-center h-[100vh]"
-          // style={{ paddingTop: "100px" }}
+          className="flex items-center justify-center"
+          style={{ minHeight: "calc(100vh - 201px)" }}
         >
           <div
           className="flex items-center justify-center w-full h-full flex-col"
@@ -21,7 +21,7 @@ const MyList = () => {
             <p className="text-[#f9f9f9] text-[15px] py-2 pb-2 ">El contenido que agregues aparecerá aquí.</p>
           </div>
         </div>
-      ) : <div style={{ paddingTop: "70px",  paddingLeft: "calc(3.5vw + 24px)", paddingRight: "calc(3.5vw + 24px)"}}>
+      ) : <div style={{ paddingTop: "70px",  paddingLeft: "calc(3.5vw + 24px)", paddingRight: "calc(3.5vw + 24px)", minHeight: "calc(100vh - 201px)"}}>
         <h1 className=" text-[44px] text-[#f9f9f9] pb-6 py-10">Mi lista</h1>
         <h4 className=" text-xl text-[#f9f9f9] pb-5">Mis películas y series</h4>
       <div
